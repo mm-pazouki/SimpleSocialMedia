ad0c97c0970ca3937382d735d55420ff49325df7
Mr Pazouki <mm_pazouki@yahoo.com>
2023-02-05 15:57:03 +0330

    Widgets

--- account/forms.py
+++ account/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 
 class UserRegistrationForm(forms.Form):
-    username = forms.CharField()
-    email = forms.EmailField()
-    password = forms.CharField()
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your Username'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Your Email'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Your Password'}))
+
