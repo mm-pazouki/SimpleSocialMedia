60b3c62cba31ec38b8b9302436f68824bfd9270d
Mr Pazouki <mm_pazouki@yahoo.com>
2023-02-10 18:47:56 +0330

    error 404

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -17,14 +17,14 @@ class HomeView(View):
 class PostDetailView(View):
 
     def get(self, request, post_id, post_slug):
-        post = Post.objects.get(id=post_id, slug=post_slug)
+        post = get_object_or_404(Post, pk=post_id, slug=post_slug)
         return render(request, 'home/detail.html', {'post': post})
 
 
 class PostDeleteView(LoginRequiredMixin, View):
 
     def get(self, request, post_id):
-        post = Post.objects.get(pk=post_id)
+        post = get_object_or_404(Post, pk=post_id, slug=post_slug)
         if post.user.id == request.user.id:
             post.delete()
             messages.success(request, 'Post deleted successfully', 'success')
@@ -37,7 +37,7 @@ class PostUpdateView(LoginRequiredMixin, View):
     form_class = PostCreateUpdateForm
 
     def setup(self, request, *args, **kwargs):
-        self.post_instance = Post.objects.get(pk=kwargs['post_id'])
+        self.post_instance = get_object_or_404(Post, pk=kwargs['post_id'])
         return super().setup(request, *args, **kwargs)
 
     def dispatch(self, request, *args, **kwargs):
