399d1f8caf4fbd60cebc69093e5a7cc06a7f8817
Mr Pazouki <mm_pazouki@yahoo.com>
2023-02-10 19:12:43 +0330

    password reset email

--- account/views.py
+++ account/views.py
@@ -3,9 +3,10 @@ from django.views import View
 from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, views as auth_views
 from django.contrib.auth.mixins import LoginRequiredMixin
 from home.models import Post
+from django.urls import reverse_lazy
 
 
 class UserRegisterView(View):
@@ -74,3 +75,8 @@ class UserProfileView(LoginRequiredMixin, View):
         posts = Post.objects.filter(user=user)
         return render(request, 'account/profile.html', {'user': user, 'posts': posts})
 
+
+class UserPasswordResetView(auth_views.PasswordResetView):
+    template_name = 'account/password_reset_form.html'
+    success_url = reverse_lazy('account:password_reset_done')
+    email_template_name = 'account/password_reset_email.html'
