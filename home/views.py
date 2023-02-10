fa76bf879ce104872a27a8eb2515073ada37ccb2
Mr Pazouki <mm_pazouki@yahoo.com>
2023-02-10 17:43:52 +0330

    delete post

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 
 
 class HomeView(View):
@@ -17,3 +19,13 @@ class PostDetailView(View):
         return render(request, 'home/detail.html', {'post': post})
 
 
+class PostDeleteView(LoginRequiredMixin ,View):
+
+    def get(self, request, post_id):
+        post = Post.objects.get(pk=post_id)
+        if post.user.id == request.user.id:
+            post.delete()
+            messages.success(request, 'Post deleted successfully', 'success')
+        else:
+            messages.error(request, 'You cant delete this post', 'danger')
+        return redirect('home:home')
