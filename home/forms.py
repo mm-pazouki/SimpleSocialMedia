2b66d1a6ad41f07006c9a75880b8122982371a26
Mr Pazouki <mm_pazouki@yahoo.com>
2023-02-11 17:15:13 +0330

    Create Replies

--- home/forms.py
+++ home/forms.py
@@ -15,3 +15,10 @@ class CommentCreateForm(forms.ModelForm):
         widgets = {
             'body': forms.Textarea(attrs={'class': 'form-control'})
         }
+
+
+class CommentReplyForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body', )
+
