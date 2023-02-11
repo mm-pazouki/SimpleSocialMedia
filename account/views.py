eca7b422319bff2aad59789fa77153f3ac7fdf74
Mr Pazouki <mm_pazouki@yahoo.com>
2023-02-11 15:18:34 +0330

    Next

--- account/views.py
+++ account/views.py
@@ -39,6 +39,10 @@ class UserLoginView(View):
     form_class = UserLoginForm
     template_name = 'account/login.html'
 
+    def setup(self, request, *args, **kwargs):
+        self.next = request.GET.get('next')
+        return super().setup(request, *args, **kwargs)
+
     def dispatch(self, request,*args, **kwargs):
         if request.user.is_authenticated:
             return redirect('home:home')
@@ -56,6 +60,8 @@ class UserLoginView(View):
             if user is not None:
                 login(request, user)
                 messages.success(request, 'You logged in successfully', 'success')
+                if self.next:
+                    return redirect(self.next)
                 return redirect('home:home')
             messages.error(request, 'Username or password is wrong', 'warning')
         return render(request, self.template_name, {'form': form})
